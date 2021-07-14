8b9723f71c24ae76e0ac90a03b243648a044f2f9
Mateus Hernandes <mateushernandes939@gmail.com>
2021-07-14 10:41:07 -0300

    feat: create route signup

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,3 +4,7 @@ def init_app(app: Flask):
     from .teste_view import bp as bp_teste
 
     app.register_blueprint(bp_teste)
+
+    from .signup_view import bp as bp_signup
+
+    app.register_blueprint(bp_signup)
