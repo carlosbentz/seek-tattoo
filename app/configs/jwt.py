6050d9fa6f1483f0c6e6f95d3c6d3d229f8cb41f
carlos bentz <carlos.bentz@hotmail.com>
2021-07-15 23:03:53 +0000

    Revert "Merge branch 'ana_adress' into 'master'"
    This reverts merge request !12

--- app/configs/jwt.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_jwt_extended import JWTManager
-
-
-def init_app(app: Flask):
-
-    JWTManager(app)
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -4,11 +4,3 @@ def init_app(app: Flask):
     from .teste_view import bp as bp_teste
 
     app.register_blueprint(bp_teste)
-
-    from .signup_view import bp as bp_signup
-
-    app.register_blueprint(bp_signup)
-
-    from .login_view import bp as bp_login
-
-    app.register_blueprint(bp_login)
